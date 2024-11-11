6374c455bc81fd06c6fadff96ead77a35bb44527
김민석 <als981209@gmail.com>
2024-11-11 11:35:09 +0900

    DB 위치 수정 , styled-components 적용

--- frontend/src/components/FilledButton.js
+++ frontend/src/components/FilledButton.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import './FilledButton.css';
+import styled from 'styled-components';
 
 const FilledButton = ({
   children,
-  color = 'blue',
+  color = 'primary_55',
   onClick,
   size = 'md',
 }) => {
   return (
-    <button
-      className={`button-container ${color} ${size}`}
-      onClick={onClick}
-    >
+    <ButtonContainer color={color} onClick={onClick} size={size}>
       {children}
-    </button>
+    </ButtonContainer>
   );
 };
 
 export default FilledButton;
+
+const ButtonContainer = styled.button<{ size: CustomSize }>`
+  cursor: pointer;
+  background-color: ${({ theme, color }) => theme.color[color]};
+  color: white;
+  font-size: ${({ theme }) => theme.font.size.body_1};
+  font-weight: ${({ theme }) => theme.font.weight.medium};
+  width: ${({ size }) => customWidth(size)};
+  height: ${({ size }) => customHeight(size)};
+
+  border-radius: 4px;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
