faf16943af7903c56cbbc2e986fc6a9f31765b5b
Kim minseok <80530044+98minseok@users.noreply.github.com>
2024-11-12 17:37:24 +0900

    git Test

---
+++ frontend/src/components/company/ContentTitle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+function ContentTitle({children}) {
+    return(
+        <Container>
+            {children}
+        </Container>
+    );
+}
+
+export default ContentTitle;
+
+const Container = styled.span`
+    
+    font-size : 35px;
+`
