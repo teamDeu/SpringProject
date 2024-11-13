96fae939bd2fa235e3f9d3f95df7af579f323e38
ParkJungHyun1233 <149235089+ParkJungHyun1233@users.noreply.github.com>
2024-11-13 10:31:38 +0900

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './container';
 import Test from './container/test';
-<<<<<<< HEAD
 import Login from './container/log/login';
 import Member from './container/log/member';
 import Member2 from './container/log/member2';
@@ -11,15 +10,11 @@ import Findid from './container/log/findid';
 import Findpwd from './container/log/findpwd';
 import Rpwd from './container/log/rpwd';
 
-=======
-import Login from './container/login';
-import Member from './container/member';
-import Member2 from './container/member2';
-import Findid from './container/findid';
+
 import GlobalStyle from './GlobalStyles';
 import GlobalFont from './GlobalFont';
 import InputCompanyInfo from './container/inputCompanyInfo'
->>>>>>> 192e5820742e7543e303aa80cd333163df6ef123
+
 function App() {
 
   return (
@@ -33,20 +28,16 @@ function App() {
             <Route path="member" element={<Member/>}/>
             <Route path="member2" element={<Member2/>}/>
             <Route path="findid" element={<Findid/>}/>
-<<<<<<< HEAD
+
             <Route path="findpwd" element={<Findpwd/>}/>
             <Route path="rpwd" element={<Rpwd/>}/>
-=======
+
             <Route path="inputCompanyInfo" element ={<InputCompanyInfo/>}/>
->>>>>>> 192e5820742e7543e303aa80cd333163df6ef123
+
           </Routes>
         </BrowserRouter>
   );
 }
 
 export default App;
-<<<<<<< HEAD
-=======
 
-  
->>>>>>> 192e5820742e7543e303aa80cd333163df6ef123
