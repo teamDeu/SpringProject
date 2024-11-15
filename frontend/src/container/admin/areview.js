ed28a5ef18af6a4b859a02975c2f4932c83dab37
parkeunhyo <eun261126@naver.com>
2024-11-15 17:15:23 +0900

    관리자

--- frontend/src/container/admin/areview.js
+++ frontend/src/container/admin/areview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Menu from '../../components/admin/Menu';
 import PageHeader from '../../components/admin/PageHeader';
-
+import DropdownSelect from "../../components/admin/Select";
 
 const Areview = () => {
     return (
@@ -9,6 +9,17 @@ const Areview = () => {
             <Menu />
             <div style={{ padding: '20px', flex: 1, position: 'relative' }}>
                 <PageHeader title="리뷰" />
+                <div className="member-header">
+                        <DropdownSelect
+                        initialOptions={["회원ID", "작성일", "합격유무", "리뷰(기업명)", "첨부파일", "등록현황"]}
+                        defaultOption="회원ID"
+                        onChange={(selectedOption) =>
+                            console.log("Selected member type:", selectedOption)
+                        }
+                        showPlusButton={false}
+                        showDeleteButton={false}
+                        />
+                </div>
             </div>
         </div>
     );
