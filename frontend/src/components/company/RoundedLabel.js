3cf5c1f194272e7d50dea13022993a723b4be16a
Kim minseok <80530044+98minseok@users.noreply.github.com>
2024-11-21 18:12:47 +0900

    companymanagepost DB연동

--- frontend/src/components/company/RoundedLabel.js
+++ frontend/src/components/company/RoundedLabel.js
@@ -11,6 +11,6 @@ export default RoundedLabel
 const Label = styled.div`
   color : ${props => props.color};
   padding : 10px 20px;
-  border : 1px solid black;
+  border : 1px solid #B5B5B5;
   border-radius : 30px;
 `
