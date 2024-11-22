78526236384f3de8469d47e20490ce3f5f7a9f46
tpdud66770 <tpdud66770@naver.com>
2024-11-22 09:45:02 +0900

    11.222

--- Backend/src/main/java/com/example/Backend/repository/ELocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Backend.repository;
-
-import com.example.Backend.model.ELocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ELocationRepository extends JpaRepository<ELocation, Long> {
-}
--- Backend/src/main/java/com/example/Backend/repository/ResumeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Backend.repository;
-
-import com.example.Backend.model.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-}
